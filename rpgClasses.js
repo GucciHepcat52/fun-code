e1986bc94bf853f60aa25cfe47b3de28d8c05fc3
Dallin Breen <dallinbreen624@gmail.com>
2022-02-26 14:16:18 -0700

    added new functionality to warrior class

--- rpgClasses.js
+++ rpgClasses.js
@@ -1,29 +1,40 @@
 // This code is here to represent various classes that are found in video games
 
 class Class {
-    constructor(name, gender, race) {
-        this.name = name;
-        this.gender = gender;
-        this.race = race;
-        this.healthBar = 100;
-    }
+  constructor(name, gender, race) {
+    this.name = name;
+    this.gender = gender;
+    this.race = race;
+    this.healthBar = 100;
+  }
+
+  introduction() {
+    return `I am a mighty force in Azeroth. My name you will know me as is ${this.name}`;
+  }
 }
 
 class Warrior extends Class {
-    constructor(name, gender, race) {
-        super(name, gender, race);
-        this.rageBar = 0;
-    }
+  constructor(name, gender, race) {
+    super(name, gender, race);
+    this.rageBar = 0;
+  }
+
+  rageMeter(num) {
+    let newMeter = num + this.rageBar;
+    return `I used Lightning Strike on my foe. My rage has now grown to ${newMeter}.`;
+  }
 }
 
 class Mage extends Class {
-    constructor(name, gender, race) {
-        super(name, gender, race);
-        this.manaBar = 100;
-    }
+  constructor(name, gender, race) {
+    super(name, gender, race);
+    this.manaBar = 100;
+  }
 }
 
-const lorthar = new Warrior('Lorthar', 'male', 'nord');
+const lorthar = new Warrior("Lorthar", "male", "nord");
 console.log(lorthar);
-const misty = new Mage('Misty', 'female', 'human');
-console.log(misty);
+console.log(lorthar.rageMeter(50));
+console.log(lorthar.introduction());
+// const misty = new Mage('Misty', 'female', 'human');
+// console.log(misty);
